c143a4dae4a4835000c80e155f9fc0837c347179
Christian G. Warden <cwarden@xerus.org>
2014-08-01 09:24:50 -0700

    Use _id as timestamp for removing old documents

--- scripts/purge-old-documents.js
+++ scripts/purge-old-documents.js
@@ -1,7 +1,16 @@
+// From http://stackoverflow.com/a/8753670/120731
+var objectIdFromTimestamp = function(timestamp) {
+	// Convert date object to hex seconds since Unix epoch
+	var hexSeconds = Math.floor(timestamp/1000).toString(16);
+
+	// Create an ObjectId with that hex timestamp
+	return ObjectId(hexSeconds + "0000000000000000");
+};
+
 var now = new Date();
 // Remove documents more than 30 days old
 var old = new Date(now - 1000*60*60*24*30);
 var query = {
-	t: { $lt: old }
+	_id: { $lt: objectIdFromTimestamp(old) }
 };
 db.mobile_events.remove(query);
